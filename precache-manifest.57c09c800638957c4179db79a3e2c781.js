d5f1bc6315ca2e5bd1d19a526e45dba5a0ce099c
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-06-03 19:43:39 +0200

    Updates

---
+++ precache-manifest.57c09c800638957c4179db79a3e2c781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47357c2c47c31a3787daf6a89f13c39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b64c7e510531238313",
+    "url": "/static/css/main.143a83a0.chunk.css"
+  },
+  {
+    "revision": "dce923fd34eb761d572f",
+    "url": "/static/js/2.4b9feb22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4b9feb22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b64c7e510531238313",
+    "url": "/static/js/main.ebd6db45.chunk.js"
+  },
+  {
+    "revision": "dcc0b4f20f878b8f48d3",
+    "url": "/static/js/runtime-main.2d6c1a4b.js"
+  },
+  {
+    "revision": "baa58c3c16b76d6e733e5f658ec2a3f8",
+    "url": "/static/media/image.baa58c3c.png"
+  },
+  {
+    "revision": "25f44a895ddcec20b3c1b7eeac904f9f",
+    "url": "/static/media/piotrSliwka.25f44a89.jpg"
+  }
+]);
