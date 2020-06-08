ebeee2ae15b486902301f16d9f4d1f14cf77609d
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-06-08 20:37:53 +0200

    Updates

---
+++ precache-manifest.929b746d822a0278062c0255a3e43af4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332fd9e29c17bb909e3d88cde0c13e79",
+    "url": "/schedule-your-meal/index.html"
+  },
+  {
+    "revision": "a553a46ae2e04f7699fa",
+    "url": "/schedule-your-meal/static/css/main.389c2520.chunk.css"
+  },
+  {
+    "revision": "ab217a592d38d64c6876",
+    "url": "/schedule-your-meal/static/js/2.47f7df42.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/schedule-your-meal/static/js/2.47f7df42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a553a46ae2e04f7699fa",
+    "url": "/schedule-your-meal/static/js/main.a7c46c7b.chunk.js"
+  },
+  {
+    "revision": "29884a2c42358977b4de",
+    "url": "/schedule-your-meal/static/js/runtime-main.05fc21d1.js"
+  },
+  {
+    "revision": "baa58c3c16b76d6e733e5f658ec2a3f8",
+    "url": "/schedule-your-meal/static/media/image.baa58c3c.png"
+  },
+  {
+    "revision": "25f44a895ddcec20b3c1b7eeac904f9f",
+    "url": "/schedule-your-meal/static/media/piotrSliwka.25f44a89.jpg"
+  }
+]);
