727c82de90a18b2596bd386a553ae33ba2de33b6
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-06-19 13:09:41 +0200

    Updates

--- precache-manifest.3fad9613b9bfe1039569d5f93941a698.js
+++ precache-manifest.3fad9613b9bfe1039569d5f93941a698.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f79b7f9f57b84236e84f1e0d88a986c2",
+    "revision": "a215592c4bf2069d605c1e71a000bdcc",
     "url": "/schedule-your-meal/index.html"
   },
   {
-    "revision": "d6345069490e95856648",
+    "revision": "af5c28626b66ac07f971",
     "url": "/schedule-your-meal/static/css/main.e091e9d3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/schedule-your-meal/static/js/2.e6dda84a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d6345069490e95856648",
-    "url": "/schedule-your-meal/static/js/main.4db1a670.chunk.js"
+    "revision": "af5c28626b66ac07f971",
+    "url": "/schedule-your-meal/static/js/main.e2e07c29.chunk.js"
   },
   {
     "revision": "29884a2c42358977b4de",
