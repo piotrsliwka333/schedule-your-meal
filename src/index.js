1f0a9cb0d32cdf4d9be39d1fa7baa226a1332998
Peter Sliwka <piotrsliwka333@gmail.com>
2022-03-20 13:07:58 +0100

    Update firebase

--- src/index.js
+++ src/index.js
@@ -8,16 +8,17 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCfnJbfmd62J8vKl0Ql0aKxeAKUQQResIM",
-	authDomain: "schedule-your-meal.firebaseapp.com",
-	databaseURL: "https://schedule-your-meal.firebaseio.com",
-	projectId: "schedule-your-meal",
-	storageBucket: "schedule-your-meal.appspot.com",
-	messagingSenderId: "730957919059",
-	appId: "1:730957919059:web:1daea59ee84e98859967a6",
-	measurementId: "G-CQ4JCTD811"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-firebase.initializeApp(firebaseConfig)
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
 	<App/>,
